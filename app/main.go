5ab51576a6e1ac86933bac72e49b68fb5467d3e7
sekijum <Jumpei0910@icloud.com>
2023-01-28 21:46:19 +0900

    おちんちん

---
+++ app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	// "github.com/gin-gonic/gin"
+	// "github.com/sekijum/go-dev-01/app/presentation/controller"
+)
+
+func main() {
+	fmt.Println("hoge")
+	// controller.Intro()
+	// r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// r.Run() // 0.0.0.0:8080 でサーバーを立てます。
+}
